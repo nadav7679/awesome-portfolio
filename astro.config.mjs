843754cff02cdcd1354445afcc3dc3580df849ab
nadav7679 <nadav76799@gmail.com>
2023-08-14 12:06:41 +0300

    Added latex and mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), mdx()],
+  markdown: {
+      remarkPlugins: [
+        remarkMath,
+    ],
+    rehypePlugins: [
+        [rehypeKatex, {
+        // Katex plugin options
+        }]
+    ]
+  }
 });
